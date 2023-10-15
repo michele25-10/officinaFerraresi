35b4ad263d8d2877ef7670b1dc3504a214f53e64
michele25-10 <michele.gabri01@gmail.com>
2023-10-15 20:30:19 +0200

    Iniziato form richiedi preventivo

--- officina-ferraresi/src/components/richiediPreventivo/richiediPreventivo.tsx
+++ officina-ferraresi/src/components/richiediPreventivo/richiediPreventivo.tsx
@@ -5,11 +5,85 @@ export default function RichiediPreventivo() {
     <>
       <div className="richiedi-preventivo mb-5">
         <div className="titolo-paragrafo">
-          <h2 className="text-center text-white">Richiedi preventivo</h2>
+          <h2 className="text-center ">Richiedi preventivo</h2>
         </div>
         <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
-          <div className="p-5">
-            <img className="img" src="./Preventivo.jpg"></img>
+          <div className="grid justify-center">
+            <div className="container mx-auto p-5">
+              <img
+                className="img-preventivo object-cover"
+                src="./Preventivo.jpg"
+              ></img>
+            </div>
+          </div>
+          <div className="grid justify-center">
+            <form>
+              <div className="form-control">
+                <label htmlFor="nome" className="label">
+                  <span className="label-text">Nome</span>
+                </label>
+                <input
+                  type="text"
+                  id="nome"
+                  name="nome"
+                  placeholder="Nome"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label htmlFor="cognome" className="label">
+                  <span className="label-text">Cognome</span>
+                </label>
+                <input
+                  type="text"
+                  id="cognome"
+                  name="cognome"
+                  placeholder="Cognome"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label htmlFor="mail" className="label">
+                  <span className="label-text">Mail</span>
+                </label>
+                <input
+                  type="email"
+                  id="mail"
+                  name="mail"
+                  placeholder="Mail"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label htmlFor="numero" className="label">
+                  <span className="label-text">Numero telefono</span>
+                </label>
+                <input
+                  type="text"
+                  id="numero"
+                  name="numero"
+                  placeholder="Numero"
+                  className="input input-bordered"
+                  maxLength={10}
+                />
+              </div>
+              <div className="form-control">
+                <label htmlFor="richiesta" className="label">
+                  <span className="label-text">Richiesta</span>
+                </label>
+                <textarea
+                  id="richiesta"
+                  name="richiesta"
+                  placeholder="Richiesta"
+                  className="input input-bordered"
+                  maxLength={250}
+                />
+              </div>
+              <div className="policy"></div>
+              <div className="mt-3 text-center">
+                <button className="btn btn-error">Invia</button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
